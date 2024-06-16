023c601fa5ab153978341fdb39129fb27815c378
Oscar Castro <112106599+ocastrobeltran@users.noreply.github.com>
2024-06-16 01:39:11 +0000

    Ajuste de ruta de imagenes en objeto de js de problemas algoritmicos

--- scripts/problemasAlgoritmicos.js
+++ scripts/problemasAlgoritmicos.js
@@ -254,13 +254,13 @@ const objetoEjercicios = {
   "titulo3": "Ejercicio 3",
   "titulo4": "Ejercicio 4",
   "pregunta1": "Desarrollar un programa que permita calcular el área o perímetro de algunas figuras planas (triángulo, rectángulo, cuadrado, círculo). El programa debe solicitar al usuario el tipo de figura y las dimensiones necesarias para realizar el cálculo.",
-  "codigo1": "/img/problemasAlgoritmicos/codigoEjercicio1.png",
+  "codigo1": "/evidenciasADSO/img/problemasAlgoritmicos/codigoEjercicio1.png",
   "pregunta2": "Desarrollar un programa que permita almacenar las edades de un grupo de 10 personas en un vector de enteros y luego determine la cantidad de personas que son menores de edad, mayores de edad, cuántos adultos mayores, la edad más baja, la edad más alta y el promedio de edades ingresadas. Para el ejercicio anterior suponga que un adulto mayor debe tener una edad igual o superior a 60. Debe validar para cada ingreso que los valores estén en un rango entre 1 y 120 años. En caso de error deberá notificar y solicitar un nuevo valor.",
-  "codigo2": "/img/problemasAlgoritmicos/codigoEjercicio2.png",
+  "codigo2": "/evidenciasADSO/img/problemasAlgoritmicos/codigoEjercicio2.png",
   "pregunta3": "Escriba un programa que lea dos vectores de números enteros ordenados ascendentemente y luego produzca la lista ordenada de la mezcla de los dos, por ejemplo, si los dos arreglos tienen los números 1 3 6 9 17 y 2 4 10 17, respectivamente, la lista de números en la pantalla debe ser 1 2 3 4 6 9 10 17 17. Limite los vectores a un tamaño de 5 y debe validar en cada ingreso que realmente se estén ingresando los datos de forma ascendente.",
-  "codigo3": "/img/problemasAlgoritmicos/codigoEjercicio3.png",
+  "codigo3": "/evidenciasADSO/img/problemasAlgoritmicos/codigoEjercicio3.png",
   "pregunta4": "Una emisora con presencia en diferentes ciudades desea conocer el rating de canciones y cantantes más escuchados (sonados) en este semestre del año. Por lo tanto, se ha pedido a estudiantes del SENA del programa de tecnólogo en análisis y desarrollo de software desarrollar una solución que permita conocer la respuesta de 6 personas con relación a sus gustos musicales. Con fines administrativos y realizar una rifa entre las personas encuestadas, la emisora desea poder registrar de las personas entrevistadas su nombre, número de identificación (cédula), fecha de nacimiento, correo electrónico, ciudad de residencia, ciudad de origen. Además, se deberá poder almacenar el artista y título de hasta 3 canciones favoritas en cada una de las personas que se ingrese, teniendo en cuenta lo anterior, se sugiere que la solución deberá mostrar un menú que permite las siguientes opciones: a. Agregar una persona con los datos que se listan anteriormente. b. Mostrar la información personal de una persona particular por medio de su posición en el vector.",
-  "codigo4": "/img/problemasAlgoritmicos/codigoEjercicio4.png",
+  "codigo4": "/evidenciasADSO/img/problemasAlgoritmicos/codigoEjercicio4.png",
 }
 
 function mostrarEjercicio1() {
